2ce2271a9fb6e0839fb726b67ee91f2dd5a4dbe8
civalwar114 <sly114@naver.com>
2021-07-19 11:42:39 +0900

    Initial commit

---
+++ Assets/ColumnNew.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnNew : MonoBehaviour {
+    public GameControlNew gameControl;
+    Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+
+        gameControl = FindObjectOfType<GameControlNew>();
+
+        rb2D = GetComponent<Rigidbody2D>();
+
+        rb2D.velocity = new Vector2(-2, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (gameControl.gameOver == true)
+        {
+            rb2D.velocity = Vector2.zero;
+
+
+        }
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Bird>() != null)
+        {
+
+            gameControl.BirdSocred();
+
+        }
+
+    }
+
+
+}
---
+++ Assets/Flappy Bird Style/Scripts/GameControlNew.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControlNew : MonoBehaviour {
+    public GameObject gameoverui;
+    public Text scoreText;
+    public bool gameOver = false;
+    // Use this for initialization
+
+    int score = 0;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (gameOver == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(0);
+
+            }
+        }
+
+	}
+    // 사망 처리 함수
+    public void BirdDied()
+    {
+        //게임 오버 ui 호출
+        gameoverui.SetActive(true);
+
+        gameOver = true;
+
+    }
+
+    public void BirdSocred()
+    {
+        score = score + 1;
+        //ui 표시
+        scoreText.text = "Score : " + score.ToString();
+
+    }
+
+}
